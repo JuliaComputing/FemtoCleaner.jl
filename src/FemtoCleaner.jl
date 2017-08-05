abd3385990bd3569df20e396a575ea4d1a28136c
Keno Fischer <keno@juliacomputing.com>
2017-08-05 17:54:19 -0400

    More work on interactions

--- src/FemtoCleaner.jl
+++ src/FemtoCleaner.jl
@@ -66,101 +66,7 @@ function dry_run(repo_url)
     end
 end
 
-function with_pr_branch(f, repo, auth)
-    repo_url = "https://x-access-token:$(auth.token)@github.com/$(get(repo.full_name))"
-    local_dir = mktempdir()
-    try
-        lrepo = LibGit2.clone(repo_url, local_dir)
-        LibGit2.branch!(lrepo, "fbot/deps", track=LibGit2.Consts.REMOTE_ORIGIN)
-        f((lrepo, local_dir))
-    finally
-        rm(local_dir, force=true, recursive=true)
-    end
-end
-
-function byte_range_for_line(text, line)
-    lines = split(text, '\n')
-    first = sum(sizeof, lines[1:line-1]) + line
-    first:(first+sizeof(lines[line])-1)
-end
-
-function first_expr_in_range(tree, range)
-    for c in children(tree)
-        first(range) in c.fullspan || continue
-        first(c.span) in range && return c
-        return first_expr_in_range(c, range)
-    end
-    error("No expression found in range")
-end
-
-function delete_expression_at_line(path, line)
-    text = readstring(path)
-    p = Deprecations.overlay_parse(text, true)
-    @assert !isexpr(p, CSTParser.ERROR)
-    range = byte_range_for_line(text, line)
-    @show line
-    @show range
-    @show text[range]
-    expr = first_expr_in_range(p, range - 1)
-    open(path, "w") do f
-        data = Vector{UInt8}(text)
-        splice!(data, 1 + expr.fullspan)
-        write(f, data)
-    end
-end
-
-function respond(repo::Repo, rev::Review, c::Comment, auth)
-    if get(c.body) == "delete this entirely"
-        with_pr_branch(repo, auth) do x
-            lrepo, local_dir = x
-            diff_hunk = get(c.diff_hunk)
-            # @@ -155,7 +155,7 @@
-            buf = IOBuffer(diff_hunk)
-            skip(buf, 2)
-            readuntil(buf, '+')
-            startline = parse(Int, readuntil(buf, ',')[1:end-1])
-            path = joinpath(local_dir, get(c.path))
-            # The comment occurs before get(c.position) so after
-            # get(c.position)-1
-            delete_expression_at_line(path, startline+get(c.position)-1)
-            LibGit2.add!(lrepo, get(c.path))
-            LibGit2.commit(lrepo, "Address review comments")
-            LibGit2.push(lrepo, refspecs = ["HEAD:refs/heads/fbot/deps"])
-        end
-        return true
-    else
-        GitHub.reply_to(repo, rev, c, "I'm sorry, I don't know how to do that :anguished:."; auth = auth)
-        return false
-    end
-end
-
-function pr_response(event)
-    # New review on a pull request
-    #   Was this pull request opened by us
-    pr = PullRequest(event.payload["pull_request"])
-    get(get(pr.user).login) == "femtocleaner[bot]" || return
-    #   Was the new review for changes requested?
-    r = GitHub.Review(pr, event.payload["review"])
-    get(r.state) == "changes_requested" || return
-    # Authenticate
-    jwt = GitHub.JWTAuth(4123, joinpath(Pkg.dir("FemtoCleaner"), "femtocleaner.2017-07-30.private-key.pem"))
-    installation = Installation(event.payload["installation"])
-    auth = create_access_token(installation, jwt)
-    # Look through the comments for this review
-    repo = Repo(event.payload["repository"])
-    cs, page_data = comments(repo, r)
-    results = map(c->respond(repo, r, c, auth), cs)
-    while haskey(page_data, "next")
-        cs, page_data = comments(r, start_page = page_data["next"])
-        isempty(cs) && return
-        append!(results, map(c->respond(repo, r, c, auth), cs))
-    end
-    if all(results)
-        create_comment(repo, pr, "I have addressed the review comments."; auth=auth)
-    elseif any(results)
-        create_comment(repo, pr, "I have addressed all the review comments I know how to."; auth=auth)
-    end
-end
+include("interactions.jl")
 
 function event_callback(event)
     # On installation, process every repository we just got installed into
@@ -169,6 +75,13 @@ function event_callback(event)
         installation = Installation(event.payload["installation"])
         auth = create_access_token(installation, jwt)
         for repo in event.payload["repositories"]
+            apply_deprecations(GitHub.repo(GitHub.Repo(repo)), auth)
+        end
+    elseif event.kind == "installation_repositories"
+        jwt = GitHub.JWTAuth(4123, joinpath(Pkg.dir("FemtoCleaner"), "femtocleaner.2017-07-30.private-key.pem"))
+        installation = Installation(event.payload["installation"])
+        auth = create_access_token(installation, jwt)
+        for repo in event.payload["repositories_added"]
             apply_deprecations(GitHub.repo(GitHub.Repo(repo)), auth)
         end
     elseif event.kind == "pull_request_review"
---
+++ src/interactions.jl
@@ -0,0 +1,144 @@
+using Deprecations: apply_formatter, changed_text, TextReplacement
+
+struct FileTextReplacement
+    path::String
+    repl::TextReplacement
+end
+FileTextReplacement(path::String, args...) = FileTextReplacement(path, TextReplacement(args...))
+
+function with_pr_branch(f, repo, auth)
+    repo_url = "https://x-access-token:$(auth.token)@github.com/$(get(repo.full_name))"
+    local_dir = mktempdir()
+    try
+        lrepo = LibGit2.clone(repo_url, local_dir)
+        LibGit2.branch!(lrepo, "fbot/deps", track=LibGit2.Consts.REMOTE_ORIGIN)
+        f((lrepo, local_dir))
+    finally
+        rm(local_dir, force=true, recursive=true)
+    end
+end
+
+function byte_range_for_line(text, line)
+    lines = split(text, '\n')
+    first = sum(sizeof, lines[1:line-1]) + line
+    first:(first+sizeof(lines[line])-1)
+end
+
+function first_expr_in_range(tree, range)
+    for c in children(tree)
+        first(range) in c.fullspan || continue
+        first(c.span) in range && return c
+        return first_expr_in_range(c, range)
+    end
+    error("No expression found in range")
+end
+
+function with_node_at_line(f, path, line)
+    text = readstring(path)
+    p = Deprecations.overlay_parse(text, true)
+    @assert !isexpr(p, CSTParser.ERROR)
+    range = byte_range_for_line(text, line)
+    expr = first_expr_in_range(p, range - 1)
+    f(expr)
+end
+
+function delete_expression_at_line(path, line)
+    with_node_at_line(path, line) do expr
+        FileTextReplacement(path, expr.fullspan, "")
+    end
+end
+
+function align_arguments(path, line)
+    with_node_at_line(path, line) do expr
+        FileTextReplacement(path,
+            apply_formatter(Deprecations.format_align_arguments, expr))
+    end
+end
+
+function respond(repo::Repo, rev::Review, c::Comment, auth, actions)
+    # Figure out what line we're at
+    line = begin
+        diff_hunk = get(c.diff_hunk)
+        buf = IOBuffer(diff_hunk)
+        skip(buf, 2)
+        readuntil(buf, '+')
+        @show c
+        println(diff_hunk)
+        startline = parse(Int, readuntil(buf, ',')[1:end-1])
+        startline + count(c->c[1] == ' ' || c[1] == '+',
+            split(diff_hunk, '\n')[2:end]) - 1
+    end
+    @show line
+    if get(c.body) == "delete this entirely"
+        push!(actions, (local_dir, resolutions)->begin
+            path = joinpath(local_dir, get(c.path))
+            push!(resolutions, delete_expression_at_line(path, line))
+        end)
+        return true
+    elseif get(c.body) == "align arguments"
+        push!(actions, (local_dir, resolutions)->begin
+            path = joinpath(local_dir, get(c.path))
+            push!(resolutions, align_arguments(path, line))
+        end)
+        return true
+    elseif get(c.body) == "bad bot"
+        # TODO: File issue on this repository
+        GitHub.reply_to(repo, rev, c, "I'm sorry. I'm still new at this :monkey:. I'll file an issue about this for you."; auth = auth)
+        return false
+    else
+        GitHub.reply_to(repo, rev, c, "I'm sorry, I don't know how to do that :anguished:."; auth = auth)
+        return false
+    end
+end
+
+function pr_response(event)
+    # New review on a pull request
+    #   Was this pull request opened by us
+    pr = PullRequest(event.payload["pull_request"])
+    get(get(pr.user).login) == "femtocleaner[bot]" || return
+    #   Was the new review for changes requested?
+    r = GitHub.Review(pr, event.payload["review"])
+    get(r.state) == "changes_requested" || return
+    # Authenticate
+    jwt = GitHub.JWTAuth(4123, joinpath(Pkg.dir("FemtoCleaner"), "femtocleaner.2017-07-30.private-key.pem"))
+    installation = Installation(event.payload["installation"])
+    auth = create_access_token(installation, jwt)
+    # Look through the comments for this review
+    repo = Repo(event.payload["repository"])
+    cs, page_data = comments(repo, r)
+    @show length(cs)
+    actions = Any[]
+    results = map(c->respond(repo, r, c, auth, actions), cs)
+    while haskey(page_data, "next")
+        cs, page_data = comments(repo, r, start_page = page_data["next"])
+        isempty(cs) && return
+        append!(results, map(c->respond(repo, r, c, auth, actions), cs))
+    end
+    resolutions = Any[]
+    if !isempty(actions)
+        with_pr_branch(repo, auth) do x
+            lrepo, local_dir = x
+            for a in actions
+                a(local_dir, resolutions)
+            end
+            paths = unique(map(r->r.path, resolutions))
+            for path in paths
+                text = readstring(joinpath(local_dir, path))
+                open(joinpath(local_dir, path), "w") do f
+                    changes = collect(map(x->x.repl,
+                        filter(x->x.path == path, resolutions)))
+                    @show (path, changes)
+                    write(f, changed_text(text, changes)[2])
+                end
+                LibGit2.add!(lrepo, relpath(path, local_dir))
+            end
+            LibGit2.commit(lrepo, "Address review comments")
+            LibGit2.push(lrepo, refspecs = ["HEAD:refs/heads/fbot/deps"])
+        end
+    end
+    if all(results)
+        create_comment(repo, pr, "I have addressed the review comments."; auth=auth)
+    elseif any(results)
+        create_comment(repo, pr, "I have addressed all the review comments I know how to."; auth=auth)
+    end
+end
